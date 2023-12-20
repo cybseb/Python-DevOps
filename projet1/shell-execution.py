70ea968d573d0f1fdc6c5094e1acdc090da6053d
Sebastien <125477634+cybseb@users.noreply.github.com>
2023-12-20 12:27:24 +0100

    Ajout projet2

--- projet1/shell-execution.py
+++ projet1/shell-execution.py
@@ -8,3 +8,13 @@ try:
 	print(result.stdout)
 except subprocess.CalledProcessError as e:
 	print(f"Erreur : {e}")
+
+import os
+import subprocess
+# Script execution without parameters
+script_dir = os.path.dirname(__file__)
+script_abosulte_path = os.path.join( script_dir, "Files/script.sh")
+subprocess.call(['sh', script_abosulte_path])
+# Script execution with parameters
+param_script_abosulte_path = os.path.join( script_dir, "Files/parm.sh")
+subprocess.call(['sh', param_script_abosulte_path, 'param1 param2'])
